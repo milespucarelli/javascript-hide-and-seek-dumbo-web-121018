e52e23e3e1a168280bfdc0b86bcd6cab1299ec75
milespucarelli <miles.pucarelli@gmail.com>
2019-01-30 19:52:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('.ranked-list li');
+  for(let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let next = node.children[0];
+  while (next) {
+    node = next;
+    next = next.children[0];
+  }
+  return node;
+}
+
+function recurse(elements) {
+  if (element.innerText === 'boo!') {
+    return element;
+  } else {
+    recurse(element);
+  }
+}
